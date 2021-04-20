0598406b19449c8e4e127ea8ca27640746d424d5
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2021-04-20 17:38:06 +0200

    background-silder

--- background-slider/script.js
+++ background-slider/script.js
@@ -1,11 +1,37 @@
-const body = document.body;
-const slides = document.querySelectorAll('.slide');
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
 
-let activeSilde = 0;
- setBgToBody();
+
+let activeSilde = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSilde++;
+    if(activeSilde > slides.length - 1){
+        activeSilde = 0
+    }
+    setBgToBody()
+    setActiveSlide()
+})
+
+
+leftBtn.addEventListener('click', () => {
+    activeSilde--
+    if(activeSilde < 0){
+        activeSilde = slides.length -1 
+    }
+    setBgToBody()
+    setActiveSlide()
+})
 
 function setBgToBody() {
-    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+    body.style.backgroundImage = slides[activeSlide].style.
+    backgroundImage;
 }
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'))
+    slides[activeSilde].classList.add('active')
+}
+
