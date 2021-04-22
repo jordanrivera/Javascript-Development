2f24f3fbdac13db8f9be2ef67a9642cb320b9381
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2021-04-22 16:39:27 +0200

    kinetik-css-loader

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,7 +1,17 @@
 const canvas = document.getElementById('canvas')
-const ctx = convas.getContext('2d')
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearEl = document.getElementById('clear')
 
-let size = 20
+
+
+
+
+const ctx = canvas.getContext('2d')
+
+let size = 10
 let isPressed = false
 let color = 'black'
 let x  
@@ -25,6 +35,9 @@ canvas.addEventListener('mousemove', (e) => {
         const y2 = e.offsetY
         drawCircle(x2,y2)
         drawLine(x2, y2)
+        x = x2
+        y = y2
+
     }
 })
 
@@ -39,8 +52,30 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
 }
+function updateSizeOnScreen(){
+    sizeEl.innerText  = size
+}
+increaseBtn.addEventListener('click', () => {
+    size += 5
 
+    if(size > 50) {
+        size = 50
+    }
+    updateSizwOnScreen()
+})
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+    updateSizwOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
 
